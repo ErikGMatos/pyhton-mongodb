2fd56c8b931d5f9416101e60d2c0bdd7ec1bf6ce
Erik Garces Matos <erik_senac@hotmail.com>
2024-12-16 21:11:18 -0300

    feat: Implementing select actions

--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -1,4 +1,5 @@
 
+from bson.objectid import ObjectId
 from pymongo.database import Database
 
 
@@ -16,3 +17,37 @@ class OrdersRepository:
         collection = self.__db_connection.get_collection(
             self.__collection_name)
         collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(
+            self.__collection_name)
+        data = collection.find(doc_filter)
+        return data
+
+    def select_one(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(
+            self.__collection_name)
+        data = collection.find_one(doc_filter)
+        return data
+
+    def select_many_with_properties(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(
+            self.__collection_name)
+        data = collection.find_one(
+            doc_filter,  # filtro de busca
+            {"_id": 0, "cupom": 0}  # opcao de retorno
+        )
+        return data
+
+    def select_if_property_exists(self, doc_filter: str) -> dict:
+        collection = self.__db_connection.get_collection(
+            self.__collection_name)
+        query = {doc_filter: {"$exists": True}}
+        data = collection.find(query, {"_id": 0, "cupom": 0})
+        return data
+
+    def select_by_object_id(self, object_id: str) -> dict:
+        collection = self.__db_connection.get_collection(
+            self.__collection_name)
+        data = collection.find_one({"_id": ObjectId(object_id)})
+        return data
--- src/models/repository/repository_test.py
+++ src/models/repository/repository_test.py
@@ -21,3 +21,87 @@ def test_insert_list_of_documents():
     orders_repository = OrdersRepository(conn)
     my_doc = [{"elem1": "aqui1"}, {"elem2": "aqui12"}, {"elem3": "aqui3"}]
     orders_repository.insert_list_of_documents(my_doc)
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_many():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {"cupom": True}
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+    for doc in response:
+        print()
+        print(doc)
+        print()
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_one():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {"cupom": True}
+    response = orders_repository.select_one(doc_filter)
+    print()
+    print(response)
+    # for doc in response:
+    #     print()
+    #     print(doc)
+    #     print()
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_many_with_properties():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {"cupom": True}
+    response = orders_repository.select_many_with_properties(doc_filter)
+    print(response)
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_if_property_exists():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = "address"
+    response = orders_repository.select_if_property_exists(doc_filter)
+    print(response)
+    for doc in response:
+        print(doc)
+        print()
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_many_with_multiple_filter():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {"cupom": True, "itens.refrigerante": {
+        "$exists": True}}  # Selmelhante a uma busca AND em SQL
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+    for doc in response:
+        print()
+        print(doc)
+        print()
+
+
+@pytest.mark.skip(reason="Interaction with db")
+def test_select_many_with_or_filter():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "$or": [
+            {"address": {"$exists": True}},
+            {"itens.doce.tipo": "chocolate"}
+        ]
+    }  # Selmelhante a uma busca OR em SQL
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+    for doc in response:
+        print()
+        print(doc)
+        print()
+
+
+def test_selct_by_object_id():
+    orders_repository = OrdersRepository(conn)
+    object_id = "676088f7722215e9728d4a7c"
+    response = orders_repository.select_by_object_id(object_id)
+    print(response)
